3977e0d1b1d29ce1d73b1ee261aa212970105f69
taron.poghosyan@yandex.ru <taron.poghosyan@yandex.ru>
2022-10-08 00:12:29 +0300

    ДЗ4 надо решить 5 задач

---
+++ task4.py
@@ -0,0 +1,16 @@
+# Задана натуральная степень k. Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена и записать в файл многочлен степени k.
+
+# Пример:
+
+# k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
+
+from random import randint
+from sympy import symbols
+from math import prod
+ 
+max_v=100
+k = int(input('Введите натуральная степень k: '))
+# коэфф. при старшей степени не должен быть равен 0
+kfc=[randint(-max_v ,max_v) for i in range(k)]+[randint(1,max_v)]
+x = symbols('x')
+print (sum(map(prod,zip(kfc,[x**i for i in range(k+1)]))), '= 0')
---
+++ task5.py
@@ -0,0 +1,23 @@
+# Даны два файла, в каждом из которых находится запись многочлена. 
+# Задача - сформировать файл, содержащий сумму многочленов. 
+# 2*x² + 4*x + 5 3*x² +10*x + 6 Вывод: 5*x² + 14*x + 11
+
+with open('pol_01.txt', 'w', encoding='utf-8') as file:
+    file.write('2*x^2 + 5*x^5')
+with open('pol_02.txt', 'w', encoding='utf-8') as file:
+    file.write('23*x^4 + 9*x^6')
+
+with open('pol_01.txt','r') as file:
+    pol_01 = file.readline()
+    list_pol_01 = pol_01.split()
+
+
+with open('pol_02.txt','r') as file:
+    pol_02 = file.readline()
+    list_pol_02 = pol_02.split()
+
+print(f'{list_pol_01} + {list_pol_02}')
+sum_poly = list_pol_01 + list_pol_02
+
+with open('sum_pol.txt', 'w', encoding='utf-8') as file:
+    file.write(f'{list_pol_01} + {list_pol_02}')
